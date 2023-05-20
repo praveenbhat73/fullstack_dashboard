5b4dc9758f9cc8dd339c6d88ec252f380192a48b
praveenbhat73 <bhatpraveen721@gmail.com>
2023-05-21 01:15:30 +0530

    intial

---
+++ server/index.js
@@ -0,0 +1,13 @@
+//  installed packages -> nodemon, dotenv, morgan, helmet, express, body-parser, mongoose, cors 
+// import packages
+// const express=require('express');
+// const helmet=require('helmet');
+// const cors =require('cors');
+//just include in package.json file type:"module" to import the files like below  
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
