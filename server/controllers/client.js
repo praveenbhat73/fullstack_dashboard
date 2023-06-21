1931a45974c4a461b2d20a44f8042b48b25f4c79
praveenbhat73 <bhatpraveen721@gmail.com>
2023-06-21 19:16:54 +0530

    minor change in server

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,7 +2,7 @@ import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
-import getCountryIso3 from "country-iso-2-to-3";
+import getCountryIso3 from  "country-iso-2-to-3";
 
 export const getProducts = async (req, res) => {
   try {
@@ -102,3 +102,6 @@ export const getCustomers = async (req, res) => {
       res.status(404).json({ message: error.message });
     }
   };
+
+
+  
